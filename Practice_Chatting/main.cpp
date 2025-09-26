6778d7cb297e7b028b25ecc9fa1443d145d3e779
ParkEunJe <prk4659@naver.com>
2025-09-26 14:05:18 +0900

    create basic chatting server, client

---
+++ Practice_Chatting/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+// 辑滚
+
+int main(void)
+{
+	WSAData wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		std::cout << "WSAStartup err";
+		return -1;
+	}
+
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (listenSocket == INVALID_SOCKET)
+	{
+		std::cout << "listenSocket err";
+		return -1;
+	}
+
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htons(INADDR_ANY);
+	serverAddr.sin_port = htons(8888);
+
+	if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		std::cout << "bind err";
+		return -1;
+	}
+
+	if (listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		std::cout << "listen err";
+		return -1;
+	}
+
+	SOCKADDR_IN clientAddr;
+	int addrlen = sizeof(clientAddr);
+	memset(&clientAddr, 0, sizeof(clientAddr));
+	SOCKET ClientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &addrlen);
+	if (ClientSocket == INVALID_SOCKET)
+	{
+		std::cout << "Accept Error: " << WSAGetLastError() << std::endl;
+		closesocket(listenSocket);
+		WSACleanup();
+		return -1;
+	}
+
+	std::cout << "立加己傍";
+	char recvBuf[1024] = { 0, };
+	char sendBuf[1024] = { 0, };
+	int recvsize = recv(ClientSocket, recvBuf, sizeof(recvBuf), NULL);
+	if (recvsize == 0)
+	{/*俊矾贸府*/
+	}
+
+	int sendsize = send(ClientSocket, recvBuf, recvsize, NULL);
+
+	closesocket(listenSocket);
+	closesocket(ClientSocket);
+	WSACleanup();
+}
---
+++ Practice_Client/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <winsock2.h>
+#include <mswsock.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main(void)
+{
+	WSAData wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, NULL, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = htons(8888);
+
+	int connectRetval = connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+	if (connectRetval != 0)
+	{
+		std::cout << "connect error!" << std::endl;
+	}
+
+
+	while (1)
+	{
+		char message[256]; // 채팅 메시지 버퍼 (최대 255자)
+		std::cout << "Enter message: ";
+		std::cin.getline(message, sizeof(message)); // 한 줄 입력 받기
+
+		char sendBuffer[1024];
+		memcpy(sendBuffer, &message, sizeof(message));
+		send(clientSocket, sendBuffer, strlen(message), 0);
+
+	}
+
+	closesocket(clientSocket);
+	WSACleanup();
+}
