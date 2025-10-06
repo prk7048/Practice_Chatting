36c21420507a1596bbbc360dd9ff0ed2c1631f59
ParkEunJe <prk4659@naver.com>
2025-10-07 08:13:31 +0900

    commit

--- Practice_Client/main_client.cpp
+++ Practice_Client/main_client.cpp
@@ -3,10 +3,11 @@
 #include <winsock2.h>
 #include <mswsock.h>
 #include <process.h>
+#include "protocol.h" // protocol.h 포함
 
-#include "protocol.h"
 #pragma comment(lib,"ws2_32.lib")
-// 스레드가 실행할 함수
+
+// --- 수신(Recv) 전용 스레드 ---
 unsigned int __stdcall RecvThread(void* pArguments)
 {
 	SOCKET clientSocket = (SOCKET)pArguments;
@@ -14,18 +15,23 @@ unsigned int __stdcall RecvThread(void* pArguments)
 
 	while (true)
 	{
+		// 서버로부터 오는 패킷을 받는다. (지금은 헤더를 분석하지 않음)
 		int recvsize = recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
 		if (recvsize <= 0)
 		{
 			std::cout << "서버와의 연결이 끊겼습니다." << std::endl;
+			// 여기서 프로그램을 강제 종료하거나, main 스레드에 종료 신호를 보내야 합니다.
+			// 지금은 간단하게 recv 루프만 탈출합니다.
 			break;
 		}
 
-		recvBuf[recvsize] = '\0';
-		std::cout << "\n[상대방]: " << recvBuf << std::endl;
-		std::cout << "Enter message: ";
-	}
+		// 받은 것이 채팅 패킷이라고 가정하고 내용물을 출력한다.
+		ChatMessagePacket* receivedPacket = (ChatMessagePacket*)recvBuf;
+		receivedPacket->message[recvsize - sizeof(PacketHeader)] = '\0'; // 널 문자 처리
 
+		std::cout << "\n[상대방]: " << receivedPacket->message << std::endl;
+		std::cout << "Enter message: "; // 다음 입력을 위해 프롬프트 재출력
+	}
 	return 0;
 }
 
@@ -38,50 +44,48 @@ int main(void)
 	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
 	SOCKADDR_IN serverAddr;
-	memset(&serverAddr, NULL, sizeof(serverAddr));
+	memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = htons(8888);
 
-	int connectRetval = connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
-	if (connectRetval != 0)
+	if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) != 0)
 	{
 		std::cout << "connect error!" << std::endl;
 		return -1;
 	}
 
+	std::cout << "server connected!" << std::endl;
 
-	std::cout << "server connet!" << std::endl;
+	// 수신 스레드 생성
 	HANDLE hRecvThread = (HANDLE)_beginthreadex(NULL, 0, RecvThread, (void*)clientSocket, 0, NULL);
 
+	// --- 송신(Send) 전용 메인 스레드 ---
 	while (true)
 	{
-		char message[256] = { 0, }; // 버퍼를 0으로 초기화하는 습관
+		char message[256] = { 0, };
 		std::cout << "Enter message: ";
 		std::cin.getline(message, sizeof(message));
 
 		if (strlen(message) > 0)
 		{
-			// 1. 보낼 패킷을 생성하고 내용을 채운다.
 			ChatMessagePacket chatPacket;
 			chatPacket.header.packetType = PacketType::CHAT_MESSAGE;
-			strcpy_s(chatPacket.message, message); // 안전한 문자열 복사 함수 사용
-
-			// 2. 헤더에 실제 패킷 크기를 기록한다.
-			//    헤더 크기 + 메시지 문자열 길이 (+1은 널 문자를 위해)
+			strcpy_s(chatPacket.message, message);
 			chatPacket.header.packetSize = sizeof(PacketHeader) + strlen(chatPacket.message) + 1;
 
-			// 3. 패킷 전체를 서버로 전송한다.
-			//    char*로 형변환하여 바이트 스트림으로 취급한다.
 			send(clientSocket, (const char*)&chatPacket, chatPacket.header.packetSize, 0);
 		}
 
-		// "q"를 입력하면 종료
 		if (strcmp(message, "q") == 0)
 		{
 			break;
 		}
 	}
+
+	// 스레드가 끝날 때까지 잠시 기다려주는 것이 좋습니다.
+	WaitForSingleObject(hRecvThread, INFINITE);
+	CloseHandle(hRecvThread);
 
 	closesocket(clientSocket);
 	WSACleanup();
